d039d1fba09b82416b82cd47d634def645310056
tomKPZ <tomKPZ@gmail.com>
2021-05-02 11:50:33 -0700

    refactoring still

--- build.py
+++ build.py
@@ -7,92 +7,7 @@ import re
 import subprocess
 import sys
 
-SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-os.chdir(os.path.join(SCRIPT_DIR, 'arc-theme'))
-subprocess.check_output(['git', 'reset', '--hard'])
-
-# TARGET = (0x0d/255, 0x0f/255, 0x18/255)
-TARGET = (0x30 / 255, 0x01 / 255, 0x01 / 255)
-TARGET_ACCENT = '#00ff00'
-TARGET_FG = '#0000ff'
-
-ARC_BG_DARK = (0x38 / 255, 0x3c / 255, 0x4a / 255)
-ARC_BG_LIGHT = (0xf5 / 255, 0xf6 / 255, 0xf7 / 255)
-ARC_ACCENT = '#5294e2'
-
-
-def parse_color(color):
-    color = color.lstrip('#')
-    return [int(color[2 * i:2 * i + 2], 16) / 255 for i in range(3)]
-
-
-def format_color(rgb):
-    return '#%02x%02x%02x' % tuple(
-        min(255, int(channel * 256)) for channel in rgb)
-
-
-# Maybe make this a shift to avoid clamping?
-def transfer_function(x0, y0):
-    constant = x0 * (y0 - 1) / (y0 * (x0 - 1))
-    return lambda x: x / (x - constant * (x - 1))
-
-
-target_h, target_l, target_s = colorsys.rgb_to_hls(*TARGET)
-bg_h, bg_l, bg_s = colorsys.rgb_to_hls(*ARC_BG_DARK)
-transfer_l = transfer_function(bg_l, target_l)
-transfer_s = transfer_function(bg_s, target_s)
-
-THEME_H = 0.61475
-THEME_S = 0.12992
-
-
-def repl(m):
-    m = m.group(0)
-    if m == ARC_ACCENT:
-        return TARGET_ACCENT
-    rgb = parse_color(m)
-    h, l, s = colorsys.rgb_to_hls(*rgb)
-    if abs(h - THEME_H) > 0.05 or abs(s - THEME_S) > 0.1:
-        return m
-    h = target_h
-    l = transfer_l(l)
-    s = transfer_s(s)
-    r, g, b = colorsys.hls_to_rgb(h, l, s)
-    return format_color((r, g, b))
-
-
-for dir, dirs, files in os.walk(os.path.join('common', 'gtk-3.0', '3.24')):
-    for file in files:
-        path = os.path.join(dir, file)
-        contents = open(path).read()
-        contents = re.sub('#[0-9a-fA-F]{6}', repl, contents)
-        open(path, 'w').write(contents)
-
-FG_COLOR_NAMES = set([
-    'selected_fg_color',
-    'text_color',
-    'fg_color',
-    'error_fg_color',
-    'warning_fg_color',
-    'suggested_fg_color',
-    'destructive_fg_color',
-])
-lines = []
-for line in open(
-        os.path.join('common', 'gtk-3.0', '3.24', 'sass',
-                     '_colors.scss')).read().split('\n'):
-    if m := re.match(r'\$(\w+): (.*);$', line):
-        if m.group(1) in FG_COLOR_NAMES:
-            lines.append('$%s: %s;' % (m.group(1), TARGET_FG))
-            continue
-    lines.append(line)
-open(os.path.join('common', 'gtk-3.0', '3.24', 'sass', '_colors.scss'),
-     'w').write('\n'.join(lines))
-
-subprocess.check_call(
-    'sed -i "s/\(placeholder_text_color\) .*;/\\1 #{\\"\\" + mix(\$text_color, \$base_color, 50%)};/" common/gtk-3.0/3.24/sass/_colors-public.scss',
-    shell=True)
-
+# ----- Unthemed colors -----
 # $warning_color: #F27835;
 # $error_color: #FC4138;
 # $success_color: #73d216;
@@ -112,8 +27,101 @@ subprocess.check_call(
 # $wm_icon_unfocused_bg: if($variant == 'light' or $variant=='lighter', #B6B8C0, #666A74);
 # $wm_icon_hover_bg: if($variant == 'light' or $variant=='lighter', #7A7F8B, #C4C7CC);
 
-subprocess.check_call('rm -rf build install', shell=True)
+GTK_VERSION = '3.24'
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+os.chdir(os.path.join(SCRIPT_DIR, 'arc-theme'))
+subprocess.check_output(['git', 'reset', '--hard'])
+
+TARGET_BG = '#300101'
+TARGET_FG = '#0000ff'
+TARGET_ACCENT = '#00ff00'
+
+ARC_BG_DARK = '#383c4a'
+ARC_BG_LIGHT = '#f5f6f7'
+ARC_ACCENT = '#5294e2'
+
+
+def parse_color(color):
+    color = color.lstrip('#')
+    return [int(color[2 * i:2 * i + 2], 16) / 255 for i in range(3)]
+
+
+def format_color(rgb):
+    return '#%02x%02x%02x' % tuple(
+        min(255, int(channel * 256)) for channel in rgb)
+
+
+# Maybe make this a shift to avoid clamping?
+def transfer_function(x0, y0):
+    constant = x0 * (y0 - 1) / (y0 * (x0 - 1))
+    return lambda x: x / (x - constant * (x - 1))
+
+inverted = parse_color(TARGET_BG)[1] > parse_color(TARGET_FG)[1]
+arc_bg = parse_color(ARC_BG_DARK if inverted else ARC_BG_LIGHT)
+
+target_h, target_l, target_s = colorsys.rgb_to_hls(*parse_color(TARGET_BG))
+bg_h, bg_l, bg_s = colorsys.rgb_to_hls(*arc_bg)
+transfer_l = transfer_function(bg_l, target_l)
+transfer_s = transfer_function(bg_s, target_s)
+
+
+def is_base_color(hue, saturation):
+    BASE_H = 0.61475
+    BASE_S = 0.12992
+    THRESH_H = 0.05
+    THRESH_S = 0.10
+    return abs(hue - BASE_H) < THRESH_H and abs(saturation - BASE_S) < THRESH_S
+
+
+def repl(m):
+    m = m.group(0)
+    if m == ARC_ACCENT:
+        return TARGET_ACCENT
+    rgb = parse_color(m)
+    h, l, s = colorsys.rgb_to_hls(*rgb)
+    if not is_base_color(h, s):
+        return m
+    return format_color(
+        colorsys.hls_to_rgb(target_h, transfer_l(l), transfer_s(s)))
+
+
+gtk_dir = os.path.join('common', 'gtk-3.0', GTK_VERSION)
+COLOR_PATTERN = re.compile(r'#[0-9a-fA-F]{6}')
+for dir, dirs, files in os.walk(gtk_dir):
+    for file in files:
+        path = os.path.join(dir, file)
+        contents = open(path).read()
+        contents = COLOR_PATTERN.sub(repl, contents)
+        open(path, 'w').write(contents)
+
+FG_COLOR_NAMES = set([
+    'selected_fg_color',
+    'text_color',
+    'fg_color',
+    'error_fg_color',
+    'warning_fg_color',
+    'suggested_fg_color',
+    'destructive_fg_color',
+])
+lines = []
+COLOR_DEFINITION_PATTERN = re.compile(r'\$(\w+): (.*);$')
+colors_sass_file = os.path.join(gtk_dir, 'sass', '_colors.scss')
+for line in open(colors_sass_file):
+    if m := COLOR_DEFINITION_PATTERN.match(line):
+        if m.group(1) in FG_COLOR_NAMES:
+            lines.append('$%s: %s;\n' % (m.group(1), TARGET_FG))
+            continue
+    lines.append(line)
+open(colors_sass_file, 'w').write(''.join(lines))
+
 subprocess.check_call(
-    'meson build --prefix="$(pwd)/install" -Dthemes=gtk3 -Dvariants=dark -Dtransprency=false -Dgtk3_version=3.24',
+    'sed -i "s/\(placeholder_text_color\) .*;/\\1 #{\\"\\" + mix(\$text_color, \$base_color, 50%)};/" common/gtk-3.0/3.24/sass/_colors-public.scss',
+    shell=True)
+
+subprocess.check_call('rm -rf build install', shell=True)
+variant = 'dark' if inverted else 'lighter'
+subprocess.check_call(
+    'meson build --prefix="$(pwd)/install" -Dthemes=gtk3 -Dvariants=%s -Dtransprency=false -Dgtk3_version=%s' % (variant, GTK_VERSION),
     shell=True)
 subprocess.check_call('meson install -C build', shell=True)
